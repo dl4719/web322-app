c10cf11852e24ce1c8b86c41437660249c993469
Daniel <dlu28@myseneca.ca>
2024-10-06 18:26:17 -0400

    Created JSON files, html files, modified server.js to make the links work properly, added store-services module to server.js

---
+++ server.js
@@ -0,0 +1,54 @@
+
+let services = require('./store-services');
+
+const express = require('express');
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
+const path = require('path');
+
+app.use(express.static('public'));
+
+/// 404 Error handler
+app.use((req, res, next) => {
+    res.status(404).send("404 - We're unable to find what you're looking for...");
+})
+
+
+app.listen(HTTP_PORT, () => console.log(`Express http server listening on: ${HTTP_PORT}`));
+
+
+/// Redirects the user to the About page.
+app.get('/', (req, res) => {
+    res.redirect("/about");
+
+});
+
+
+/// Shop page
+app.get('/shop', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/shop.html'));
+    //res.send('TODO: Get all items who have published == true');
+});
+
+
+/// About page
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'));
+
+});
+
+
+/// Items page
+app.get('/items', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/items.html'));
+
+});
+
+
+/// Categories page
+app.get('/categories', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/categories.html'));
+
+});
+
+
